3f9bdb13cd60510936faf3aa1cd886f41bddbba4
mouhaa_md <mmdiagne@ept.sn>
2024-08-14 16:05:59 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Thank you for your message! We will get back to you soon.');
+    });
